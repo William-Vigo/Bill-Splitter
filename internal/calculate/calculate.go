8dc45923f340515cdc9810c5c96ac2e82f785f39
William-Vigo <17559768+William-Vigo@users.noreply.github.com>
2021-04-03 15:20:39 -0400

    sum for each person is calculated

--- internal/calculate/calculate.go
+++ internal/calculate/calculate.go
@@ -1,5 +1,10 @@
 package calculate
 
+import (
+	"fmt"
+	"sync"
+)
+
 type Payload struct {
 	Group  []People      `json:"people"`
 	Shared []SharedItems `json:"sharedItems"`
@@ -20,4 +25,61 @@ type SharedItems struct {
 		Name string `json:"name"`
 	} `json:"people"`
 	Purchases []Items `json:"items"`
+}
+
+//TODO: calculate tax & tip
+func Process(data Payload) {
+	moneyOwed := make(map[string]float64)
+
+	var wg sync.WaitGroup
+	var lock sync.RWMutex
+	// this sums individuals total
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for _, person := range data.Group {
+			wg.Add(1)
+			go func(person People) {
+				defer wg.Done()
+				for _, purchases := range person.Purchases {
+					lock.Lock()
+					moneyOwed[person.Name] += purchases.Price
+					lock.Unlock()
+				}
+			}(person)
+		}
+	}()
+
+	if len(data.Shared) != 0 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for _, group := range data.Shared {
+				wg.Add(1)
+				go func(group SharedItems) {
+					defer wg.Done()
+
+					splitSize := len(group.People)
+					total := 0.00
+					for _, purchaces := range group.Purchases {
+						total += purchaces.Price
+					}
+
+					moneyDue := total / float64(splitSize)
+					//TODO round money to 2 decimal places
+
+					for _, people := range group.People {
+						lock.Lock()
+						moneyOwed[people.Name] += moneyDue
+						lock.Unlock()
+					}
+
+				}(group)
+
+			}
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(moneyOwed)
 }
